fc2033f5a54cae490e4676fd6c2ffb738b7c99c0
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2022-12-01 22:31:51 -0500

    navigation issue resolved

--- app/src/main/java/com/example/edublog/MainActivity.kt
+++ app/src/main/java/com/example/edublog/MainActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.edublog.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -20,15 +20,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
-        setContentView(binding.root)
+        val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        ActivityMainBinding.bind(binding.root)
 
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContatiner) as NavHostFragment
         navController = navHostFragment.navController
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        NavigationUI.setupWithNavController(bottomNavigationView, navController)
-//        setupWithNavController(bottomNavigationView, navController)
-//        setupActionBarWithNavController(navController)
+        setupWithNavController(bottomNavigationView, navController)
+
 
     }
 
